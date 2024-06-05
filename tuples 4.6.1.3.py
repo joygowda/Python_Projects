179ce406f48629b8a0a27fd351ba171ed9ba3f0c
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-05 19:41:16 -0400

    Update tuples 4.6.1.3.py

--- tuples 4.6.1.3.py
+++ tuples 4.6.1.3.py
@@ -1,4 +1,6 @@
-﻿my_tuple = (1, 10, 100)
+#code by joy d.gowda for Edube Python Essentials 1 course (Aligned with PCEP-30-02)
+
+my_tuple = (1, 10, 100)
 
 t1 = my_tuple + (1000, 10000)
 t2 = my_tuple * 3

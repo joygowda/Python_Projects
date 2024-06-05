549df666701863808e8ea16cc8367a432650f034
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-05 19:43:19 -0400

    Update list with unique integers correct code.py

--- list with unique integers correct code.py
+++ list with unique integers correct code.py
@@ -1,12 +1,13 @@
-﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+#code by joy d.gowda
+
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
 output = ()
-#
+
 for int in my_list:
     if int in output:
         output = output + ch
         
 output = my_list
-# Write your code here.
-#
+
 print("The list with unique elements only:")
 print(my_list)

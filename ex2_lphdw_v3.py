0f4e49e54b04f6111eea2031073debb0a656ab8c
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-27 10:34:59 -0400

    Add files via upload

---
+++ ex2_lphdw_v3.py
@@ -0,0 +1,16 @@
+"""
+
+Exercises from book Learn Python the Hard Way, version 3, adapted to Python 3
+by Joy Dhairyalakshmi Gowda
+I never got the message from 2011, but I may have figured it out somewhat
+
+"""
+
+# a comment,this is so you can read this program later
+#anything after # is ignored by python
+
+print("I could have code like this.") #and the comment after is ignored
+
+#you can also use a commet to disable or comment out a peice f code.
+#print("This code will not run.")
+print("This code will run!")
---
+++ ex3_lphdw_v3.py
@@ -0,0 +1,18 @@
+"""
+Exercises from book Learn Python the Hard Way, version 3, adapted to Python 3
+by Joy Dhairyalakshmi Gowda
+I never got the message from 2011, but I may have figured it out somewhat
+They gave Raj Victoria's mother that year because she was considered "used up" and families didn't matter to them.
+
+"""
+print("I will now count my chickens:")
+
+print("Hens:", 25 + 30 / 6)
+print("Roosters:", 100 - 25 * 3 % 4 )
+print("Now I will count the eggs:")
+print((3 + 2 + 1 - 5 )+ (4 % 2) - 1 / (4 + 6))
+print("Is is true that 3 + 2 < 5 - 7?")
+print( 3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)
+
+

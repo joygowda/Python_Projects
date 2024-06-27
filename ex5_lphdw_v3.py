8367207442b8348c361334d69c9295d887797431
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-27 13:21:12 -0400

    Add files via upload
    version2 created dictionary. challenging to find correct syntax for f strings that will work in my IDE and doesn't include AI help so I can learn.

--- ex5_lphdw_v3.py
+++ ex5_lphdw_v3.py
@@ -2,38 +2,33 @@
 Exercises from book Learn Python the Hard Way, version 3, adapted to Python 3
 by Joy Dhairyalakshmi Gowda
 
-I never got the message from 2011, but I may have figured it out PART of story somewhat.
+I never got the message from 2011, but I may have figured out PART of story somewhat.
 They gave someone wrong woman but she used my name and SSN that year to plot revenge, since families didn't really matter to them and they didn't want Hindu family reunited.
 They kept me from working since 2018 with gossip on Facebook.
 They knew I tested for programming aptitude in 1999 but resented me for ability to earn $.
 Vaughn F. said it was Anne Keller who ran brothels BUT I wasn't ever her!
-I worry because I think my kids were tricked to trash a good mother, but nobody would admit it. 
+I worry because I think my kids were tricked to trash a good mother, but nobody would tell me. 
 
 """
+#data from book includes
+#my_name = 'Zed A. Shaw'
+#my_age = 35 #not a lie
+#my_height = 74 #inches
+#my_weight = 180 #lbs
+#my_eyes = 'Blue'
+#my_teeth = 'White'
+#my_hair = 'Brown'
 
-my_name = 'Zed A. Shaw'
-my_age = 35 #not a lie
-my_height = 74 #inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
-my_teeth = 'dirty'
+#created dictionary with keys and values
+person = {'my_name': 'Zed A. Shaw', "my_age": 35, "my_height": 74, "my_weight": 180, "my_eyes": "blue", "my_teeth": "white", "my_hair": "brown"}
 
-me = f"Let's talk about" {my_name}"."
-print(me)
-tall = f"He's" {my_height} " inches tall."
-print(tall)
-heavy = f"He's" {my_weight}"pounds heavy."
-print(heavy)
+print(f"Let's talk about" {my_name}".".format(**person))
+print(f"He's"{my_height}" inches tall.").format(**person)
+print(f"He's" {my_weight}" pounds heavy.").format(**person)
 print("Actually that's not too heavy.")
-looks = f"He's got" {my_eyes}," eyes , and " {my_hair} "hair."
-print(looks)
-teeth = f"His teeth are usually " {my_teeth} "depending on the coffee."
-print(teeth)
+print(f"He's got" {my_eyes}" eyes and "{my_hair}"hair.").format(**person)
+print(f"His teeth are usually "{my_teeth}" depending on the coffee.").format(**person)
 
-#print("If I add %d, %d, and %d I get %d." %
-      #my_age, my_height, my_weight, my_age + my_height + my_weight)
 
-      #fixing f-string issues later
+#fixing f-string issues later. finding challenges in finding right syntax that will work in my iDE
 

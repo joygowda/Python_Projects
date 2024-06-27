a6b78b8ca83fd3a2e353c1950ce66cd22ee03060
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-27 10:10:08 -0400

    Add files via upload

---
+++ ex1_lphd_book.py
@@ -0,0 +1,15 @@
+"""
+Exercises from book Learn Python the Hard Way adapted to Python 3
+by Joy Dhairyalakshmi Gowda
+"""
+print("Hello World!")
+print("Hello Again!")
+print("I like typing this.")
+print("This is fun!")
+print('Yay! Printing....It is boring to wait.')
+print("I'd much rather you 'not'.")
+print('I  "said" do not touch this.')
+#study drills below
+print("I like Python programming language.")
+print("This is another line.")
+#print("This is commented out per drill in book.")

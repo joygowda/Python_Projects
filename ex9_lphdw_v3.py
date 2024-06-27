e93230b30ceacde66cc66aac10fd13b383bee77a
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-27 12:35:06 -0400

    Add files via upload

---
+++ ex9_lphdw_v3.py
@@ -0,0 +1,25 @@
+"""
+Exercises from book Learn Python the Hard Way, version 3, adapted to Python 3
+by Joy Dhairyalakshmi Gowda
+
+I never got the message from 2011, but I may have figured it out PART of story somewhat.
+They gave someone wrong woman but she used my name and SSN that year to plot revenge, since families didn't really matter to them and they didn't want Hindu family reunited.
+They kept me from working since 2018 with gossip on Facebook.
+I haven't been able to work because I talk about MY INNOCENCE despite social media gossip and mock trail.
+Why?I wrote a diary entry in 2000. I started a relationship with a programmer while I was in college to be a programmer.
+Diary entry mentioned concern over soapy diaper residue while Indian born man babysat for me to have interview in TX. It was actually soap and water, but daddy got sympathy saying I "let" oldest son get molested, but I didn't.
+He sold my degrees and identities because he didn't want anyone to see my truth!
+Ho is suspected to selling me to prevent my career as programmer or even finding female led tech company to employ me/
+
+"""
+
+days = "\nMon\nTues\nWed\nThu\nFri\nSat\nSun\n"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nAug\n"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+print("""
+There's something going on here.
+With the three double-"""
+"but it still printed.")

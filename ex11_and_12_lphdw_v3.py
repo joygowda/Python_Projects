3d93d9410657975ee05ce5cd3ba25d242eee13da
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-28 14:08:39 -0400

    Add files via upload

---
+++ ex11_and_12_lphdw_v3.py
@@ -0,0 +1,28 @@
+"""
+Exercises from book Learn Python the Hard Way, version 3, adapted to Python 3
+by Joy Dhairyalakshmi Gowda
+
+I never got the message from 2011, but I may have figured it out PART of story somewhat.
+Learn Python was part of what was needed.
+I liked computer programming most of all prior employment.
+I can work remotely to prevent gossip from interfering with my peers work.
+
+"""
+
+age = input("How old are you?")
+height = input("How tall are you in inches?")
+weight = input("How much do you weigh in pounds?")
+
+
+print(f"So, you're {age}, {height} inches tall and {weight} pounds heavy.")
+
+
+
+
+
+
+
+
+
+
+

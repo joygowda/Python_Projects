2a27f48a80e87a4f6fb8795bfc8fa5eec68f242b
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-06-28 12:45:32 -0400

    Add files via upload

---
+++ ex10_escape_characters_lphdw_v3.py
@@ -0,0 +1,60 @@
+"""
+Exercises from book Learn Python the Hard Way, version 3, adapted to Python 3
+by Joy Dhairyalakshmi Gowda
+
+I never got the message from 2011, but I may have figured it out PART of story somewhat.
+I liked computer programming most. I can work remotely to prevent gossip from interfering with my ability to work.
+
+"""
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+"""
+\<newline>   Backslash and newline ignored
+
+\\ Backslash (\)
+
+\' Single quote (')
+
+\" Double quote (")
+
+\a ASCII Bell (BEL)
+
+\b ASCII Backspace (BS)
+
+\f ASCII Formfeed (FF)
+
+\n ASCII Linefeed (LF)
+
+\r ASCII Carriage Return (CR)
+
+\t ASCII Horizontal Tab (TAB)
+
+\v ASCII Vertical Tab (VT)
+
+\ooo  Character with octal value ooo
+
+\xhh Character with hex value hh
+
+Escape sequences only recognized in string literals are:
+
+\N{name}  Character named name in the Unicode database
+
+\uxxxx   Character with 16-bit hex value xxxx
+
+\Uxxxxxxxx Character with 32-bit hex value xxxxxxxx
+"""

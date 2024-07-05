1fb0b0df741ac0e1ab5964807cbd920ddde95b7c
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-07-05 15:40:15 -0400

    Add files via upload

---
+++ ex29_lphw_book_v3.py
@@ -0,0 +1,44 @@
+"""
+Exercise 29:What If from Book: Learn Python the Hard Way v3
+Coded by Joy Dhairyalakshmi Gowda
+"""
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print(" Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved, but I'm still a cat lady.")
+
+if people > dogs:
+    print("The world is drooled on!")
+
+if people < dogs:
+    print("The world is dry! \....but it's not much fun without a dog too!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
+
+cats *= 15
+
+if people >= cats:
+    print("People are greater than or equal to cats.")
+
+if people <= cats:
+    print("People have to make sure outdoor and feral cats are spayed and neutered starting now.")
+
+is people == cats:
+    print("People are cats.")
+
+    

8d7e76dbe5ca199800ef83f64b1f4bbce418d31e
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-07-05 15:26:47 -0400

    Update ex26_lphw_book_v3.py

--- ex26_lphw_book_v3.py
+++ ex26_lphw_book_v3.py
@@ -2,7 +2,7 @@
 Exercise 26; Halfway point test from Book: Learn Python the Hard Way v3
 Coded by Joy Dhairyalakshmi Gowda
 """
-#to be added as this file can't be replaced easily
+#to be added if I can find website archived elsewhere
 
 
 

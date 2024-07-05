ab0a1bf57d777af014a88395f5edb7059ff6bed0
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-07-05 15:21:56 -0400

    Update ex26_lphw_book_v3.py

--- ex26_lphw_book_v3.py
+++ ex26_lphw_book_v3.py
@@ -1,49 +1,9 @@
 """
-Exercise 28; Boolean Practice from Book: Learn Python the Hard Way v3
+Exercise 26 Halways point test from Book: Learn Python the Hard Way v3
 Coded by Joy Dhairyalakshmi Gowda
 """
+#to be added as this file can't be replaced easily
 
-print(True and True) #1
-
-print(False and True) #2
-
-print(1 == 1 and 2 == 1) #3
-
-print("test" == "test") #4
-
-print(1 == 1 or 2 != 1) #5
-
-print(True and 1 == 1) #6
-
-print(False and 0 != 0) #7
-
-print(True and 1 == 1) #8
-
-print("test" == "testing") #9
-
-print(1 != 0 and 2 == 0) #10
-
-print("test" != "testing") #11
-
-print("test" == 1) #12
-
-print(not (True and False)) #13
-
-print(not (1 == 1 and 0 != 1)) #14
-
-print(not (10 == 1 or 1000 == 1000)) #15
-
-print(not (1 != 10 or 3 == 4)) #16
-
-print(not ("testing" == "testing" and "Zed" == "Cool Guy")) #17
-
-print(1 == 1 and not ("testing" == 1 or 1 == 0)) #18
-
-print("chunky" == "bacon" and not (3 == 4 or 3 == 3)) #19
-
-#3 == 3 and not ("testing" == "testing or "Python" =="Fun") #20
-#line above won't print without lowercase because it is reserved keyword
-print(3 == 3 and not ("testing" == "testing" or "python" == "Fun"))
 
 
 

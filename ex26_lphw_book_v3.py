69adfced07b8aeb58b87e80490be3551a39bf5c2
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-07-06 09:55:14 -0400

    Update ex26_lphw_book_v3.py

--- ex26_lphw_book_v3.py
+++ ex26_lphw_book_v3.py
@@ -1,10 +1,13 @@
 """
 Exercise 26; Halfway point test from Book: Learn Python the Hard Way v3
 Coded by Joy Dhairyalakshmi Gowda
+
+to be added if I can find website page with exam archived elsewhere
+Book is currently on 5th edition, so I saved copy of 3rd edition from landfill. I have a micro budget to spend on learning, so reserve most $ for certification exams.
+Sadly, an identity fraud got "vouches" to use my technology degrees, so was working when I couldn't get any interest in my resume. My nerdy & introvert problem because some people didn't WANT to talk to me. 
+My family didn't support college education, or me pursing college in my youth, despite high school college prep cirriculum. 
+Don't trust what my family has to say about me on Facebookor social media, since they didn't even both to know me BUT stole resumes and other stuff to get $ by being me. 
 """
-#to be added if I can find website archived elsewhere
-#Book is currently on %th edition, so I saved copy of 3rd edition from landfill. I have a micro budget to spend on learning, so reserve most $ for certification exams.
-#Sadly, an identity fraud got "vouches" to use my technology degrees, so was working when I couldn't get any interest in my resume. My nerdy & introvert problem because some people didn't WANT to talk to me. 
 
 
 

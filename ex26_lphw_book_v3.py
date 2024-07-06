661d6b4f02f8c8d055834e7d94bae98bc1055cd6
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-07-06 09:51:13 -0400

    Update ex26_lphw_book_v3.py

--- ex26_lphw_book_v3.py
+++ ex26_lphw_book_v3.py
@@ -4,7 +4,7 @@ Coded by Joy Dhairyalakshmi Gowda
 """
 #to be added if I can find website archived elsewhere
 #Book is currently on %th edition, so I saved copy of 3rd edition from landfill. I have a micro budget to spend on learning, so reserve most $ for certification exams.
-#Sadly, an identity fraud got "vouches" to use my technology degrees, so was working when I couldn't get any interest in my resume. Introvert problem. 
+#Sadly, an identity fraud got "vouches" to use my technology degrees, so was working when I couldn't get any interest in my resume. My nerdy & introvert problem because some people didn't WANT to talk to me. 
 
 
 

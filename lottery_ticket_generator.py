d5c2b4c6c75d503bade58d317c2ce29cc1d9e0c0
Joy Dhairyalakshmi <46052849+joygowda@users.noreply.github.com>
2024-07-24 11:45:46 -0400

    Add files via upload

---
+++ lottery_ticket_generator.py
@@ -0,0 +1,22 @@
+"""
+Lottery ticket generator
+Coded by Joy Dhairylaksmi Gowda
+No guarantee to win or promises, since I haven't won any games. Someone just says stuff online to harass me.
+Finished project will match Powerball draws
+Will be coded to add comparisons with Powerball API and user selected numbers and attractive display
+as I learn more Python
+"""
+
+import random
+
+
+def gen_ticket():
+	numbers = []
+	count = 0
+	while count < 6:
+		numbers.append(random.randint(1,69))
+		count += 1
+	return numbers
+
+print(gen_ticket())
+
